1760448fd4f6ad780b475173704bc5f9f5d615c1
HyrumMoses <hkm190002@utdallas.edu>
2022-11-12 19:12:14 -0600

    created projections chart

---
+++ src/screens/Projections/ProjectionsChart.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import { BarChart } from 'react-native-chart-kit'
+
+const barData = {
+  labels: ['January', 'February', 'March', 'April', 'May', 'June'],
+  datasets: [
+    {
+      data: [20, 45, 28, 80, 99, 43],
+    },
+  ],
+};
+
+const chartConfig = {
+  backgroundColor: "#e26a00",
+  backgroundGradientFrom: "#fb8c00",
+  backgroundGradientTo: "#ffa726",
+  decimalPlaces: 2, // optional, defaults to 2dp
+  color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+  labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+  style: {
+    borderRadius: 16
+  },
+  propsForDots: {
+    r: "6",
+    strokeWidth: "2",
+    stroke: "#ffa726"
+  }
+}
+
+function ProjectionsChart() {
+  return (
+    <BarChart
+      data={barData}
+      width={200}
+      height={220}
+      yAxisLabel={'$'}
+      chartConfig={chartConfig}
+    />
+  )
+}
+
+export default ProjectionsChart
