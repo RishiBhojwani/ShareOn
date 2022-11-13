0cf99d9c5f1df045f4031dd2b4daee778173893d
HyrumMoses <hkm190002@utdallas.edu>
2022-11-13 04:57:58 -0600

    created projections page

--- src/screens/Projections/ProjectionsChart.js
+++ src/screens/Projections/ProjectionsChart.js
@@ -1,37 +1,33 @@
 import React from 'react'
 import { BarChart } from 'react-native-chart-kit'
+import { Dimensions } from 'react-native'
 
 const barData = {
-  labels: ['January', 'February', 'March', 'April', 'May', 'June'],
+  labels: ['1 yr', '2 yrs', '5 yrs', '10 yrs'],
   datasets: [
     {
-      data: [20, 45, 28, 80, 99, 43],
+      data: [20, 45, 28, 50],
     },
   ],
 };
 
 const chartConfig = {
-  backgroundColor: "#e26a00",
-  backgroundGradientFrom: "#fb8c00",
-  backgroundGradientTo: "#ffa726",
+  backgroundColor: '#0a0442',
+  backgroundGradientFrom: "#0a0442",
+  backgroundGradientTo: "#0a0442",
   decimalPlaces: 2, // optional, defaults to 2dp
   color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
   labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
   style: {
-    borderRadius: 16
+    borderRadius: 16,
   },
-  propsForDots: {
-    r: "6",
-    strokeWidth: "2",
-    stroke: "#ffa726"
-  }
 }
 
 function ProjectionsChart() {
   return (
     <BarChart
       data={barData}
-      width={200}
+      width={Dimensions.get('window').width}
       height={220}
       yAxisLabel={'$'}
       chartConfig={chartConfig}
