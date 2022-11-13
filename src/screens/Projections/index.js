9d8243c857437c145798247211f2cb7af1a091d7
HyrumMoses <hkm190002@utdallas.edu>
2022-11-12 19:13:05 -0600

    added chart to projections

--- src/screens/Projections/index.js
+++ src/screens/Projections/index.js
@@ -1,45 +1,42 @@
 import { View, Text, ImageBackground, Pressable } from 'react-native';
 import styles from './styles';
-//import AntDesign from 'react-native-vector-icons/AntDesign';
+import AntDesign from 'react-native-vector-icons/AntDesign';
 import { useNavigation } from '@react-navigation/native';
+import ProjectionsChart from './ProjectionsChart';
 
 
 const ProjectionScreen = (props) => {
-    
+	const navigation = useNavigation();
 
-
-    const navigation = useNavigation();
-    return (
-        <View>
-        <Pressable
-                    style={{backgroundColor: '#93A8E1',marginTop: 30, borderRadius: 30, width: 300, height: 65, alignItems: 'center', justifyContent: 'center',}}
-                    onPress={() => navigation.navigate('Sign Up')}
-                >
-                    <Text style={{ color: 'black', fontSize: 30, margin: 10 }}>signup</Text>
-                </Pressable>
-                <Pressable
-                    style={{backgroundColor: '#93A8E1',marginTop: 30, borderRadius: 30, width: 300, height: 65, alignItems: 'center', justifyContent: 'center',}}
-                    onPress={() => navigation.navigate('Home')}
-                >
-                    <Text style={{ color: 'black', fontSize: 30, margin: 10 }}>Home</Text>
-                </Pressable>
-                <Pressable
-                    style={{backgroundColor: '#93A8E1',marginTop: 30, borderRadius: 30, width: 300, height: 65, alignItems: 'center', justifyContent: 'center',}}
-                    onPress={() => navigation.navigate('Risk')}
-                >
-                    <Text style={{ color: 'black', fontSize: 30, margin: 10 }}>Risk</Text>
-                </Pressable>
-                <Pressable
-                    style={{backgroundColor: '#93A8E1',marginTop: 30, borderRadius: 30, width: 300, height: 65, alignItems: 'center', justifyContent: 'center',}}
-                    onPress={() => navigation.navigate('Recommendation')}
-                >
-                    <Text style={{ color: 'black', fontSize: 30, margin: 10 }}>Recommendation</Text>
-                </Pressable>
-
-
-                
-                </View>
-    );
+	return (
+		<View>
+			<Pressable
+				style={{ backgroundColor: '#93A8E1', marginTop: 30, borderRadius: 30, width: 300, height: 65, alignItems: 'center', justifyContent: 'center', }}
+				onPress={() => navigation.navigate('Sign Up')}
+			>
+				<Text style={{ color: 'black', fontSize: 30, margin: 10 }}>signup</Text>
+			</Pressable>
+			<Pressable
+				style={{ backgroundColor: '#93A8E1', marginTop: 30, borderRadius: 30, width: 300, height: 65, alignItems: 'center', justifyContent: 'center', }}
+				onPress={() => navigation.navigate('Home')}
+			>
+				<Text style={{ color: 'black', fontSize: 30, margin: 10 }}>Home</Text>
+			</Pressable>
+			<Pressable
+				style={{ backgroundColor: '#93A8E1', marginTop: 30, borderRadius: 30, width: 300, height: 65, alignItems: 'center', justifyContent: 'center', }}
+				onPress={() => navigation.navigate('Risk')}
+			>
+				<Text style={{ color: 'black', fontSize: 30, margin: 10 }}>Risk</Text>
+			</Pressable>
+			<Pressable
+				style={{ backgroundColor: '#93A8E1', marginTop: 30, borderRadius: 30, width: 300, height: 65, alignItems: 'center', justifyContent: 'center', }}
+				onPress={() => navigation.navigate('Recommendation')}
+			>
+				<Text style={{ color: 'black', fontSize: 30, margin: 10 }}>Recommendation</Text>
+			</Pressable>
+			<ProjectionsChart />
+		</View>
+	);
 };
 
 export default ProjectionScreen;
