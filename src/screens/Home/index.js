d4d385d8d97e08ee96c63a8750cfc861a0cb3906
Rishi Bhojwani <rishibhojwani1532@gmail.com>
2022-11-12 22:08:41 -0600

    changed a bunch of frontend and added a data section

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,20 +1,46 @@
-import { View, Text, ImageBackground, Pressable } from 'react-native';
+import { View, Text, ImageBackground, Pressable,FlatList } from 'react-native';
+import { useState, useEffect } from 'react';
 import styles from './styles';
 import AntDesign from 'react-native-vector-icons/AntDesign';
 import { useNavigation } from '@react-navigation/native';
+import data from "../../../assets/data/users";
+import stocks from "../../../assets/data/stocks"
 
 
 const HomeScreen = (props) => {
     
+    const ItemSeperatorView = () => {
+        return (
+            <View
+                style={{ height: 0.5, width: '100%', backgroundColor: '#c8c8c8' }}
+            />
+        )
+    }
+    
 
-
+      
     const navigation = useNavigation();
     return (
         
+        <View style={{ backgroundColor: '#0a0442', height: '100%', width: '100%'}}>
+            <Text style={{color: 'white',fontSize: 40,marginLeft:50, marginTop:40}}>$</Text>
+            <Text style={{color: 'white',marginLeft:100, marginTop:-53,fontSize: 40}}>{data[1].income}</Text>
+            <Text style={{color: 'white',marginLeft:10, marginTop:100,fontSize: 20}}>Income:</Text>
+            <Text style={{color: 'white',marginLeft:10, marginTop:1,fontSize: 20}}>Unemployment:</Text>
+            <Text style={{color: 'white',marginLeft:10, marginTop:1,fontSize: 20}}>S&P:</Text>
+            <FlatList
+            data={stocks}
+            renderItem={({item}) => {
+                return (
+                    <Text style={{fontSize:50, color:"white"}}>{item.name}</Text>
+                );
+            }}
+            keyExtractor={item => item.id}
+            />
 
         <View style={{ backgroundColor: '#0a0442', height: '100%', width: '100%'}}>
         <Pressable onPress={() => navigation.navigate("Sign Up")}
-                        style={{marginTop: 700, marginLeft: 20}}>
+                        style={{marginTop: 490, marginLeft: 20}}>
                         <AntDesign name={"login"} size={30} color='white' />
                     </Pressable>
                     <Pressable onPress={() => navigation.navigate("Recommendation")}
@@ -34,7 +60,7 @@ const HomeScreen = (props) => {
                         <AntDesign name={"areachart"} size={30} color='white' />
                     </Pressable>
 
-                
+                    </View>
                 </View>
     );
 };
