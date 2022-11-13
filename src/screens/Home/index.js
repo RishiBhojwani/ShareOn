7d96dbfb86d09ea0fc5a5795fca9f9115888c984
Rishi Bhojwani <rishibhojwani1532@gmail.com>
2022-11-12 18:56:07 -0600

    new sign in page and fix error

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -10,6 +10,8 @@ const HomeScreen = (props) => {
 
     const navigation = useNavigation();
     return (
+        
+
         <View style={{ backgroundColor: '#0a0442', height: '100%', width: '100%'}}>
         <Pressable onPress={() => navigation.navigate("Sign Up")}
                         style={{marginTop: 750, marginLeft: 20}}>
@@ -19,7 +21,7 @@ const HomeScreen = (props) => {
                         style={{marginTop: -30, marginLeft: 120}}>
                         <AntDesign name={"bulb1"} size={30} color='white' />
                     </Pressable>
-                    <Pressable onPress={() => navigation.navigate("Recommendation")}
+                    <Pressable onPress={() => navigation.navigate("Risk")}
                         style={{marginTop: -29, marginLeft: 240}}>
                         <AntDesign name={"exclamation"} size={30} color='white' />
                     </Pressable>
--- src/screens/SignUp/index.js
+++ src/screens/SignUp/index.js
@@ -1,46 +1,40 @@
-import React, { useState, useEffect } from 'react';
 import { View, Text, ImageBackground, Pressable } from 'react-native';
-//import styles from './styles';
-//import AntDesign from 'react-native-vector-icons/AntDesign';
+import styles from './styles';
+import AntDesign from 'react-native-vector-icons/AntDesign';
 import { useNavigation } from '@react-navigation/native';
 
 
-const SignUpScreen = (props) => {
+const HomeScreen = (props) => {
     
 
 
     const navigation = useNavigation();
     return (
-        <View>
-        <Pressable
-                    style={{backgroundColor: '#93A8E1',marginTop: 30, borderRadius: 30, width: 300, height: 65, alignItems: 'center', justifyContent: 'center',}}
-                    onPress={() => navigation.navigate('Recommendation')}
-                >
-                    <Text style={{ color: 'black', fontSize: 30, margin: 10 }}>Recommendation</Text>
-                </Pressable>
-                <Pressable
-                    style={{backgroundColor: '#93A8E1',marginTop: 30, borderRadius: 30, width: 300, height: 65, alignItems: 'center', justifyContent: 'center',}}
-                    onPress={() => navigation.navigate('Home')}
-                >
-                    <Text style={{ color: 'black', fontSize: 30, margin: 10 }}>Home</Text>
-                </Pressable>
-                <Pressable
-                    style={{backgroundColor: '#93A8E1',marginTop: 30, borderRadius: 30, width: 300, height: 65, alignItems: 'center', justifyContent: 'center',}}
-                    onPress={() => navigation.navigate('Risk')}
-                >
-                    <Text style={{ color: 'black', fontSize: 30, margin: 10 }}>Risk</Text>
-                </Pressable>
-                <Pressable
-                    style={{backgroundColor: '#93A8E1',marginTop: 30, borderRadius: 30, width: 300, height: 65, alignItems: 'center', justifyContent: 'center',}}
-                    onPress={() => navigation.navigate('Projection')}
-                >
-                    <Text style={{ color: 'black', fontSize: 30, margin: 10 }}>Projection</Text>
-                </Pressable>
+        <ImageBackground source={require('../../../assets/logofinal.jpg')}
+        style={{marginLeft:0,height: '100%',width: '100%',}}>
+        <View >
+        <Pressable onPress={() => navigation.navigate("Home")}
+                        style={{marginTop: 750, marginLeft: 20}}>
+                        <AntDesign name={"home"} size={30} color='white' />
+                    </Pressable>
+                    <Pressable onPress={() => navigation.navigate("Recommendation")}
+                        style={{marginTop: -30, marginLeft: 120}}>
+                        <AntDesign name={"bulb1"} size={30} color='white' />
+                    </Pressable>
+                    <Pressable onPress={() => navigation.navigate("Recommendation")}
+                        style={{marginTop: -29, marginLeft: 240}}>
+                        <AntDesign name={"exclamation"} size={30} color='white' />
+                    </Pressable>
+                <Pressable onPress={() => navigation.navigate("Projection")}
+                        style={{marginTop: -35, marginLeft: 350}}>
+                        <AntDesign name={"areachart"} size={30} color='white' />
+                    </Pressable>
 
 
                 
                 </View>
+                </ImageBackground>
     );
 };
 
-export default SignUpScreen;
+export default HomeScreen;
