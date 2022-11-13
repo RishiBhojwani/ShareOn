8bb4eb71f2c0d5f65f797e06924e71af487c5eeb
Rishi Bhojwani <rishibhojwani1532@gmail.com>
2022-11-12 18:35:37 -0600

    updated styles and home page design improved

--- src/screens/Home/styles.js
+++ src/screens/Home/styles.js
@@ -5,10 +5,18 @@ const styles = StyleSheet.create({
         backgroundColor: '#93A8E1',
         marginTop: 30,
         borderRadius: 30,
-        width: 300,
+        width: 10,
         height: 65,
         alignItems: 'center',
         justifyContent: 'center',
-    }
+    },
+    homeButton: {
+        marginTop: 100,
+        marginLeft: 100,
+        alignContent: "flex-start",
+        justifyContent: "space-between"
+    },
 
 });
+
+export default styles;
--- src/screens/Risk/styles.js
+++ src/screens/Risk/styles.js
@@ -12,3 +12,4 @@ const styles = StyleSheet.create({
     }
 
 });
+ export default styles;
